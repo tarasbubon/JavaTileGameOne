e75233a1e931c0511cb5af05f8382ec551e3fd12
Taras.Bubon <desalmada1989@gmail.com>
2016-04-07 13:07:24 +0300

    v0.2

--- src/dev/tilegame/Launcher.java
+++ src/dev/tilegame/Launcher.java
@@ -1,10 +1,12 @@
 package dev.tilegame;
 
 
+import dev.tilegame.display.Display;
+
 public class Launcher
 {
     public static void main(String[] args)
     {
-
+        new Display("Title!", 300, 300);
     }
 }
